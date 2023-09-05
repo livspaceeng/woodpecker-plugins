9f94aac43d1d0c8c5606a466cc4b814c6ff77743
praveen-livspace <praveen@livspace.com>
2023-09-06 00:07:58 +0530

    grype woodpecker plugin - expose grype options as plugin settings

--- plugins/grype/script.sh
+++ plugins/grype/script.sh
@@ -1,2 +1,37 @@
 #!/bin/sh
-/bin/grype ${PLUGIN_ARGS} "${PLUGIN_SCAN_DIR}"
+
+command='/bin/grype'
+
+debug="${PLUGIN_DEBUG:-false}"
+severityCutoff="${PLUGIN_SEVERITY_CUTOFF}"
+onlyFixed="${PLUGIN_ONLY_FIXED:-false}"
+addCPESIfNone="${PLUGIN_ADD_CPES_IF_NONE:-false}"
+byCVE="${PLUGIN_BY_CVE:-false}"
+scanDir="${PLUGIN_SCAN_DIR:-.}"
+scanImage="${PLUGIN_SCAN_IMAGE}"
+scope="${PLUGIN_SCOPE}"
+
+[[ ! -z $severityCutoff  ]] && command+=" --fail-build $severityCutoff"
+[[ ! -z $scope  ]] && command+=" --scope $scope"
+
+if $onlyFixed; then
+  command+=" --only-fixed"
+fi
+if $addCPESIfNone; then
+  command+=" --add-cpes-if-none"
+fi
+if  $byCVE; then
+  command+=" --by-cve"
+fi
+if $debug; then
+  command+=" --vv"
+fi
+if [ -z $scanImage ]; then
+  command+=" $scanDir"
+else
+  command+=" $scanImage"
+fi
+echo "Executing $command"
+
+eval $command
+
