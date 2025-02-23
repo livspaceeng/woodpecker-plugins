4c1844316b594b4de7592b6ba773ede1b30c341f
praveen-livspace <praveen@livspace.com>
2025-02-23 11:28:47 +0530

    Docker image for jscpd

---
+++ plugins/jscpd/script.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+threshold="${PLUGIN_THRESHOLD:-20}"
+minTokens="${PLUGIN_MIN_TOKENS:-50}"
+pattern="${PLUGIN_PATTERN:-**/*}"
+ignore="${PLUGIN_IGNORE:-**/*.min.js,**/*.map,**/*.yaml,**/*.json}"
+scanDir="${PLUGIN_SCAN_DIR:-.}"
+
+echo '{
+        "threshold": '${threshold}',
+        "minTokens": '${minTokens}',
+        "reporters": [
+          "json"
+        ],
+        "output": "./",
+        "pattern": '${pattern}',
+        "ignore": "'${ignore}'"
+      }' > /tmp/.jscpd.json
+jscpd --exitCode 1 --config /tmp/.jscpd.json ${scanDir}
+
